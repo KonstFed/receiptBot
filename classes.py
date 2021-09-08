0c277fee2c1c83ec4a1cf583b133f0489e9c7f0d
Fil126 <g.fil@innopolis.university>
2021-09-09 03:21:42 +0800

    Receipt method is_complete added

--- classes.py
+++ classes.py
@@ -5,12 +5,16 @@ class Receipt:
         for product in products:
             self.products[product[0]] = product
         self.poll_id_list = []
-        self.participants = {}  # key - user_id, value - list of goods with ratios
+        self.participants = {}  # key - user_id, value - list of goods with ratios (product_id, ratio)
 
     def add_poll(self, poll_id: int):
         self.poll_id_list.append(poll_id)
 
     def add_product(self, user_id: int, product_id: int, ratio: float):
+        if product_id not in self.products:
+            # ERROR
+            return
+
         if user_id not in self.participants:
             self.participants[user_id] = []
 
@@ -36,3 +40,18 @@ class Receipt:
 
     def remove_product(self, user_id: int, product_id: int):
         pass  # TODO
+
+    def is_complete(self):
+        remaining_products = {}
+        for product_id in self.products:
+            remaining_products[product_id] = 1
+
+        for user_id in self.participants:
+            for product_id, ratio in self.participants[user_id]:
+                remaining_products[product_id] -= ratio
+
+        for ratio in remaining_products.values():
+            if abs(ratio) > 0.05:
+                return False
+
+        return True
