8ad0b1fbfc92397c80f438acbc7f3a43e161fd5e
Fil126 <g.fil@innopolis.university>
2021-09-10 05:16:15 +0800

    get_staus method added

--- classes.py
+++ classes.py
@@ -51,7 +51,7 @@ class Receipt:
 
         if len(self.participants[user_id]) == 0:
             self.participants.pop(user_id)
-                
+
     def is_complete(self):
         remaining_products = {}
         for product_id in self.products:
@@ -66,3 +66,30 @@ class Receipt:
                 return False
 
         return True
+
+    def get_product_ratios(self):
+        answer = {}
+        for product_id, ratio in self.participants.values():
+            if product_id not in answer:
+                answer[product_id] = 0
+            answer[product_id] += ratio
+
+        return answer
+
+    def get_status(self):
+        answer = "Owner: [{}]\n".format(self.owner_id)
+
+        for product_id in self.products:  # product: id, name, cnt, cost, date
+            answer += self.products[product_id][1] + ":"
+            found = False
+            for participant in self.participants:
+                if self.participants[participant][0] == product_id:
+                    answer += " [{}] - {},".format(participant, self.participants[participant][1])
+                    found = True
+
+            if not found:
+                answer += " no one bought this;\n"
+            else:
+                answer = answer[:-1] + ";\n"
+
+        return answer
